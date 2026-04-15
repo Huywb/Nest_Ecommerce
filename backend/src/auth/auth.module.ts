750f5c7867d197b8fc12d7b63f88d9f9d9b29910
huywb <pthhuy1306@gmail.com>
2026-04-15 13:15:31 +0700

    Setup auth Service

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,22 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
-import { JwtRefreshToken } from './strageties/refresh.stategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtStrategy } from 'src/strategies/jwt.strategy';
+import { RefreshStrategy } from 'src/strategies/refresh.strategy';
+
 @Module({
   imports: [
-    PassportModule.register({defaultStrategy:'jwt'}),
+    PassportModule.register({defaultStrategy: 'jwt'}),
     JwtModule.registerAsync({
       inject: [ConfigService],
-      useFactory: (configService : ConfigService)=>({
-        secret: configService.get<string>("JWT_SECRET"),
-        signOptions: {expiresIn: '15m'}
+      useFactory: (configService: ConfigService)=>({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions :{
+          expiresIn: '10d'
+        }
       })
     })
   ],
-  controllers: [AuthController,JwtRefreshToken],
-  providers: [AuthService]
+  providers: [AuthService,PrismaService,JwtStrategy,RefreshStrategy],
+  controllers: [AuthController]
 })
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,31 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { registerDto } from './dto/register.dto';
-import { error } from 'console';
 import bcrypt from 'bcryptjs';
-import { randomBytes } from 'crypto';
 import { JwtService } from '@nestjs/jwt';
+import { randomBytes } from 'crypto';
+import { RegisterDto } from './dto/Register.dto';
 
 @Injectable()
 export class AuthService {
-  private readonly SATL = 10;
+  private readonly SALT = 10;
   constructor(
     private readonly prisma: PrismaService,
-    private readonly jwtService: JwtService
-) {}
+    private readonly configService: ConfigService,
+    private readonly JwtService: JwtService,
+  ) {}
 
-  async register(registerDto: registerDto) {
-    const { email, password, firstName, lastName } = registerDto;
+  async register(data: RegisterDto) {
+    const { email, password, firstName, lastName } = data;
 
-    const extstUser = await this.prisma.user.findUnique({
-      where: { email },
-    });
-
-    if (extstUser) {
-      throw new Error('User already exist');
-    }
-
-    const hashPass =await bcrypt.hash(password, this.SATL);
+    const hashPass = await bcrypt.hash(password, this.SALT);
 
     const newUser = await this.prisma.user.create({
       data: {
@@ -36,23 +29,81 @@ export class AuthService {
       },
     });
 
-    const tokens = this.generateToken(newUser.id, newUser.email)
+    const tokens = await this.generateToken(newUser.id, newUser.email);
+
+    await this.updateRefreshToken(newUser.id,tokens.refreshToken)
 
     return {
-        ...tokens,
-        newUser
+        tokens,
+      newUser,
+    };
+  }
+
+  async generateToken(userId: string, email: string) {
+    const payload = { sub: userId, email };
+    const refreshId = randomBytes(15).toString('hex');
+    const [accessToken, refreshToken] = await Promise.all([
+      this.JwtService.signAsync(payload, {
+        expiresIn: '15m',
+        secret: this.configService.get<string>('JWT_SECRET'),
+      }),
+      this.JwtService.signAsync(
+        { ...payload, refreshId },
+        {
+          expiresIn: '7d',
+          secret: this.configService.get<string>('JWT_SECRET_REFRESH'),
+        },
+      ),
+    ]);
+
+    return {
+        accessToken,
+        refreshToken
     }
   }
 
-  async generateToken(userId: string, email: string){
-    const payload = {sub: userId,email}
-    const refreshId = randomBytes(15).toString('hex')
-    const [accessToken,refreshToken] = await Promise.all([
-        this.jwtService.signAsync(payload,{expiresIn: '15m'}),
-        this.jwtService.signAsync({...payload,refreshId},{expiresIn: '7d'})
-    ])
-
-    return {accessToken, refreshToken}
+  async updateRefreshToken(userId: string, refreshToken: string) {
+    const hashRefreshToken =await bcrypt.hash(refreshToken, this.SALT);
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { refreshToken: hashRefreshToken },
+    });
   }
-  
+
+  async logout(userId:string){
+    await this.prisma.user.update({where:{id: userId},data:{refreshToken: null}})
+  }
+
+  async login(login : {email:string, password:string}){
+    const checkUser = await this.prisma.user.findUnique({where:{email: login.email}})
+    if(!checkUser){
+        throw new UnauthorizedException()
+    }
+    const checkPass =await bcrypt.compare(login.password,checkUser.password)
+    if(!checkPass){
+        throw new UnauthorizedException()
+    }
+    const tokens = await this.generateToken(checkUser.id, checkUser.email)
+    await this.updateRefreshToken(checkUser.id, tokens.refreshToken)
+    return {
+        ...tokens,
+        checkUser
+    }
+  }
+
+  async refreshToken(userId: string){
+    const user = await this.prisma.user.findUnique({where:{id: userId}})
+
+    if(!user){
+        throw new UnauthorizedException()
+    }
+
+    const tokens = await this.generateToken(userId,user.email)
+    await this.updateRefreshToken(userId,tokens.refreshToken)
+
+    return {
+        ...tokens,
+        user
+    }
+  }
 }
